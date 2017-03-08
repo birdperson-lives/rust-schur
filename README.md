6ee8033b68fc4bf00acf9fcc92f2562a6b1a2633
Conrad Parker <conrad.parker.official@gmail.com>
2017-03-07 21:13:52 -0600

    Basic info on README and started doc

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# rust-schur
+# Rust Program to Calculate `Schur(5)`
+
+This program is designed to find the fifth [Schur number](https://github.com/birdperson-lives/rust-game-of-life.git).
+
+The program will also generate lower bounds on `Schur(5)` which will hopefully eventually improve on the current best lower bound of 160.
+
+# Credits
+
+* Conrad Parker
+
+### TODO
+* Document with `rustdoc`
+* Make sure program always exits upon completion
+* Restrict final solution print to single thread
--- src/spartition.rs
+++ src/spartition.rs
@@ -6,8 +6,11 @@ use std::collections::HashSet;
 use std::fs::File;
 use std::time::SystemTime;
 
+/// Unsigned integer type to use for elements of partitions. For Schur(5), u16 is will do.
 pub type Num = u16;
 
+/// Represents a partition of `[1, ..., N]` for some `N` into `k` parts, each of which is sum-free (i.e. there are no `x`,`y`,`z` in the same part such that `x + y = z`).
+/// Internally keeps track of the sums from each part (i.e. `x + y` for all `x`,`y` in the part) and the least number, if any (thus an `Option<Num>`), that all parts have elements that can sum to.
 #[derive(PartialEq, Eq, Clone)]
 pub struct SPartition {
 	top: Num,
@@ -17,18 +20,22 @@ pub struct SPartition {
 }
 
 impl SPartition {
+	/// Creates a new partition of `[1, ..., 0] = []` into `k` parts. There is only one such partition, which is the one returned.
 	pub fn new(k: usize) -> SPartition {
 		SPartition { top: 0, partition: vec![vec![]; k], sums: vec![HashSet::new(); k], cap: None}
 	}
 
+	/// Returns the number `N` for which `self` is a partition of `[1, ..., N]`. Runs in `O(1)` time.
 	pub fn top(&self) -> Num {
 		self.top
 	}
 
+	/// If there is a number `p` such that in every part there are `x` and `y` such that `x + y = p`, returns `Some(p)` for the least such `p`. Otherwise, returns `None`. Runs in `O(1)` time.
 	pub fn cap(&self) -> Option<Num> {
 		self.cap
 	}
 
+	/// Adds the next number (`self.top()+1`) to part `i`. Due to updating internal data, runs in `O(N)` time, where `N = self.top()`.
 	fn add_at(mut self, i: usize) -> SPartition {
 		self.top += 1;
 		self.partition.get_mut(i).unwrap().push(self.top);
@@ -47,6 +54,7 @@ impl SPartition {
 		self
 	}
 
+	/// 
 	pub fn find_children(&self, best: Arc<RwLock<(Num,SPartition)>>, log_file: Arc<Mutex<File>>) -> Vec<SPartition> {
 		let mut children: Vec<SPartition> = vec![];
 		for i in 0..self.partition.len() {
